3178fc7ad7676bf6c9afeb68c0fd3d041b707131
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-08-26 14:41:23 +0300

    CHECKPOINT2

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,32 @@
 // ==================== CHECKPOINT 1 - COMMENTED ====================
 
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Text('My name is Nour Al Sulais')
-      )
-    );
-}
-
-// ==================== CHECKPOINT 2 - COMMENTED ====================
 // import 'package:flutter/material.dart';
 
 // void main() {
 //   runApp(
 //     const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         body: Center(
-//           child: Text(
-//             'Welcome to SWE 463',
-//             style: TextStyle(fontSize: 24),
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
+//       home: Text('My name is Nour Al Sulais')
+//       )
+//     );
 // }
+
+// ==================== CHECKPOINT 2 - COMMENTED ====================
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Welcome to SWE 463',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    ),
+  );
+}
 
 // ==================== CHECKPOINT 3 - COMMENTED ====================
